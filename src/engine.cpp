3977e035d315a04e90faef0fd94c954aab45a112
childyouth <childyouth@agape.hanyang.ac.kr>
2025-05-22 00:02:08 +0900

    multithreading error fixed. improved readability.

--- src/engine.cpp
+++ src/engine.cpp
@@ -12,34 +12,55 @@ std::string API_TRADE_KEY;
 std::mutex output_mutex;
 
 
-engine::engine(std::string _ini_filename):ini_filename(_ini_filename), submit_done(false){
-    try
-    {
-        read_ini(ini_filename, ini_ptree);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << "\n";
-        exit(-1);
-    }
 
-    parse_app_args();
-    parse_api_info();
 
-    year_month_list = generate_year_month_list();
-    if(!year_month_list){
-        std::cerr << "ymlist" << "\n";
-        exit(-1);
+void engine::allocate_task(ldcd_t lawd_cd){
+    #pragma omp parallel for
+    for(int i = 0;i < year_month_list_len; i++){
+        ym_t deal_ymd = year_month_list[i];
+        api_msg* rent_msg = new api_msg{api_type::RENT, lawd_cd, deal_ymd}; 
+        api_msg* trade_msg = new api_msg{api_type::TRADE, lawd_cd, deal_ymd};
+        
+        push_task(rent_msg);
+        push_task(trade_msg);
     }
-
-    lawd_cd_list = parse_lawd_cd_list();
-    if(!lawd_cd_list){
-        std::cerr << "lawdlist" << "\n";
-        exit(-1);
-    }
-    
-    worker_ctx = (worker_context*)malloc(sizeof(worker_context) * num_workers);
 }
+
+void engine::allocate_work(){
+
+    {
+        std::lock_guard<std::mutex> lock(output_mutex);
+        std::cout << "[" << std::string(20,'|') << "]" << "\n";
+        std::cout << "[";
+    }
+    for (int i = 0;i<lawd_cd_list_len;i++){
+        allocate_task(lawd_cd_list[i]);
+        if(i % (lawd_cd_list_len / 20) == 0){
+            std::lock_guard<std::mutex> lock(output_mutex);
+            std::cout << "|";
+        }
+    }
+    while(!api_msg_queue.empty()){
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    {
+        std::lock_guard<std::mutex> lock(output_mutex);
+        std::cout << "|]" << "\n";
+    }
+    submit_done.store(true);
+
+    std::cout << "== all work allocated ==" << "\n";
+}
+
+void engine::init_worker_ctx(int id){
+    worker_ctx[id].id = id;
+    worker_ctx[id].is_done = false;
+    worker_ctx[id].num_working_tasks = 0;
+    worker_ctx[id].num_finished_tasks = 0;
+    worker_ctx[id].num_failed_tasks = 0;
+    worker_ctx[id].error_tasks.clear();
+}
+
 
 void engine::push_task(api_msg* api_msg_ptr){
     while(!api_msg_queue.push(api_msg_ptr));
@@ -49,16 +70,8 @@ bool engine::pull_task(api_msg*& api_msg_ptr){
     return api_msg_queue.pop(api_msg_ptr);
 }
 
-boost::asio::awaitable<void> engine::handle_api_msg(api_msg *msg, worker_context *worker_ctx)
-{
-    worker_ctx->num_working_tasks++;
-    co_await asio::steady_timer(co_await asio::this_coro::executor, std::chrono::milliseconds(1)).async_wait(asio::use_awaitable);
-    worker_ctx->num_finished_tasks++;
-    co_return;
-}
-
 bool engine::is_worker_busy(worker_context* ctx){
-    return !(ctx->num_working_tasks < WORKER_MAX_WORKLOAD);
+    return !(ctx->num_working_tasks < worker_max_workload);
 }
 
 bool engine::is_worker_busy(int thread_id){
@@ -73,12 +86,22 @@ bool engine::is_worker_working(int thread_id){
     return is_worker_working(&worker_ctx[thread_id]);
 }
 
+boost::asio::awaitable<void> engine::handle_api_msg(api_msg *msg, worker_context *worker_ctx)
+{
+    co_await asio::steady_timer(co_await asio::this_coro::executor, std::chrono::milliseconds(1)).async_wait(asio::use_awaitable);
+    
+    worker_ctx->num_working_tasks--;
+    worker_ctx->num_finished_tasks++;
+    co_return;
+}
+
+
 int engine::worker(int thread_id){
     worker_context* ctx = &worker_ctx[thread_id];
 
     asio::io_context io_context;
     api_msg* msg = NULL;
-    
+
     while(!ctx->is_done){
         // worker가 여력이 없다면
         if(is_worker_busy(ctx)){
@@ -90,6 +113,7 @@ int engine::worker(int thread_id){
             // msg queue에 처리할 task가 있다면
             if(pull_task(msg)){
                 // 코루틴 생성
+                ctx->num_working_tasks++;
                 asio::co_spawn(io_context,handle_api_msg(msg,ctx),asio::detached);
             }
             // msg queue가 비었으며 채워질 일이 없다면
@@ -101,9 +125,54 @@ int engine::worker(int thread_id){
     }
     {
         std::lock_guard<std::mutex> lock(output_mutex);
+        if(ctx->num_working_tasks){
+            std::cout << is_worker_busy(ctx) << "\n";
+            std::cout << "THREAD " << ctx->id << " : WORKING TASKS LEFT (" << ctx->num_working_tasks << ")" << "\n";
+        }
+        else{
+            std::cout << "THREAD " << ctx->id << " : NO WORKING TASKS LEFT" << "\n";
+        }
         std::cout << "THREAD " << ctx->id << " : Finished (" << ctx->num_finished_tasks << ")" << "\n";
     }
 }
+
+
+
+void engine::run(){
+    
+    asio::thread_pool pool(num_workers);
+
+    for (int i = 0;i<num_workers;i++) {
+        init_worker_ctx(i);
+
+        asio::post(pool, [this, i]() {
+            this->worker(i);
+        });
+    }
+
+    // api_msg msg{api_type::RENT, 1,1};
+    // push_task(&msg);
+    // api_msg msg2{api_type::TRADE, 1,1};
+    // push_task(&msg2);
+    // submit_done.store(true);
+    allocate_work();
+
+    pool.join();
+
+    int num_finished = 0;
+    int num_failed = 0;
+    int num_working = 0;
+    for(int i = 0;i<num_workers;i++){
+        num_working += worker_ctx[i].num_working_tasks;
+        num_finished += worker_ctx[i].num_finished_tasks;
+        num_failed += worker_ctx[i].num_failed_tasks;
+    }
+    std::cout << "STILL WORKING(should be 0) : (" << num_working << ")\n";
+    std::cout << "TOTAL Finished : (" << num_finished << ")\n";
+    std::cout << "TOTAL Failed : (" << num_failed << ")\n";
+}
+
+
 
 ym_t* engine::generate_year_month_list(){
     ym_t earliest_ymd = (ym_t)(std::stoi(ini_ptree.get<std::string>("variable.earliest_ymd","0")));
@@ -198,85 +267,44 @@ void engine::parse_app_args()
     stdcode_filename = ini_ptree.get<std::string>("engine.stdcode_filename");
     savename = ini_ptree.get<std::string>("engine.savename");
     num_workers = std::stoi(ini_ptree.get<std::string>("engine.num_thread"));
+    worker_max_workload = std::stoi(ini_ptree.get<std::string>("engine.worker_max_workload"));
 
     std::cout << std::string(40, '=') << "\n";
     std::cout << "STDCODE FILENAME : " << stdcode_filename << "\n";
     std::cout << "SAVENAME : " << savename << "\n";
     std::cout << "NUM THREAD : " << num_workers << "\n";
+    std::cout << "WORKER MAX WORKLOAD : " << worker_max_workload << "\n";
     std::cout << std::string(40, '=') << "\n";
 }
 
-void engine::allocate_task(ldcd_t lawd_cd){
-    #pragma omp parallel for
-    for(int i = 0;i < year_month_list_len; i++){
-        ym_t deal_ymd = year_month_list[i];
-        api_msg* rent_msg = new api_msg{api_type::RENT, lawd_cd, deal_ymd}; 
-        api_msg* trade_msg = new api_msg{api_type::TRADE, lawd_cd, deal_ymd};
-        
-        push_task(rent_msg);
-        push_task(trade_msg);
-    }
-}
 
-void engine::allocate_work(){
-
+engine::engine(std::string _ini_filename):ini_filename(_ini_filename), submit_done(false){
+    try
     {
-        std::lock_guard<std::mutex> lock(output_mutex);
-        std::cout << "[" << std::string(20,'|') << "]" << "\n";
-        std::cout << "[";
+        read_ini(ini_filename, ini_ptree);
     }
-    for (int i = 0;i<lawd_cd_list_len;i++){
-        allocate_task(lawd_cd_list[i]);
-        if(i % (lawd_cd_list_len / 20) == 0){
-            std::lock_guard<std::mutex> lock(output_mutex);
-            std::cout << "|";
-        }
-    }
-    while(!api_msg_queue.empty()){
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
+    catch(const std::exception& e)
     {
-        std::lock_guard<std::mutex> lock(output_mutex);
-        std::cout << "|]" << "\n";
+        std::cerr << e.what() << "\n";
+        exit(-1);
     }
-    submit_done.store(true);
 
-    std::cout << "== all work allocated ==" << "\n";
-}
+    parse_app_args();
+    parse_api_info();
 
-void engine::init_worker_ctx(int id){
-    worker_ctx[id].id = id;
-    worker_ctx[id].is_done = false;
-    worker_ctx[id].num_working_tasks = 0;
-    worker_ctx[id].num_finished_tasks = 0;
-}
+    year_month_list = generate_year_month_list();
+    if(!year_month_list){
+        std::cerr << "ymlist" << "\n";
+        exit(-1);
+    }
 
-void engine::run(){
+    lawd_cd_list = parse_lawd_cd_list();
+    if(!lawd_cd_list){
+        std::cerr << "lawdlist" << "\n";
+        exit(-1);
+    }
     
-    asio::thread_pool pool(num_workers);
-
-    for (int i = 0;i<num_workers;i++) {
-        init_worker_ctx(i);
-
-        asio::post(pool, [this, i]() {
-            this->worker(i);
-        });
-    }
-
-    // api_msg msg{api_type::RENT, 1,1};
-    // push_task(&msg);
-    allocate_work();
-
-    pool.join();
-
-    int num_finished = 0;
-    int num_failed = 0;
-    for(int i = 0;i<num_workers;i++){
-        num_finished += worker_ctx[i].num_finished_tasks;
-        num_failed += worker_ctx[i].num_failed_tasks;
-    }
-    std::cout << "TOTAL Finished : (" << num_finished << ")\n";
-    std::cout << "TOTAL Failed : (" << num_failed << ")\n";
+    worker_ctx = (worker_context*)malloc(sizeof(worker_context) * num_workers);
 }
 
 
